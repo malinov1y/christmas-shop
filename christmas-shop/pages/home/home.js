e4db1db89a77075548b4fe6ec073d71f8c100c67
Nikita Malinovsky <nikitamalinovsky3@gmail.com>
2024-12-01 22:51:52 +0300

    feat: add timer

--- christmas-shop/pages/home/home.js
+++ christmas-shop/pages/home/home.js
@@ -80,3 +80,25 @@ leftButton.addEventListener('click', function() {
 });
 
 updateSliderPosition();
+
+
+//timer
+function newYearTimer(){
+    const currentDate = new Date();
+    const newYearDate = new Date(Date.UTC(currentDate.getUTCFullYear() + 1, 0, 1));
+
+    const difference = newYearDate - currentDate;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = days;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+}
+
+setInterval(newYearTimer, 1000);
+newYearTimer();
