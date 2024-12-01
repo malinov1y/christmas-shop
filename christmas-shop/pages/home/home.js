6f2e2e7ebde5fc2ce368ea5e89dd0373030532d1
Nikita Malinovsky <nikitamalinovsky3@gmail.com>
2024-12-01 22:13:29 +0300

    feat: add slider

--- christmas-shop/pages/home/home.js
+++ christmas-shop/pages/home/home.js
@@ -1,9 +1,9 @@
+//burger-menu
 const body = document.body;
 const burgerButton = document.querySelector('.header-burger');
 const headerContainer = document.querySelector('.header-container');
 const burgerMenu = document.querySelector('.header-menu');
 const menuLinks = document.querySelectorAll('.header-menu__a');
-
 
 
 burgerButton.addEventListener('click', function() {
@@ -17,7 +17,7 @@ burgerButton.addEventListener('click', function() {
 
 menuLinks.forEach(link => {
     link.addEventListener('click', (event) => {
-        // сначала отменяем обычный переход по ссылке и закрываем меню
+        // отменяем обычный переход по ссылке и закрываем меню
         event.preventDefault();
 
         headerContainer.classList.remove('open');
@@ -29,3 +29,54 @@ menuLinks.forEach(link => {
         }, 600);
     });
 });
+
+
+//slider
+const sliderContainer = document.querySelector('.slider-items');
+const leftButton = document.querySelector('.slider-button__left');
+const rightButton = document.querySelector('.slider-button__right');
+
+let currentPosition = 0;
+
+function getClickDistance() {
+    const screenWidth = window.innerWidth;
+    let clickCount;
+    if (screenWidth >= 769) {
+        clickCount = 3;
+    } else if (screenWidth >= 380) {
+        clickCount = 6;
+    } else {
+        clickCount = 6;
+    }
+
+    const sliderWidth = sliderContainer.scrollWidth;
+    const visibleWidth = sliderContainer.offsetWidth;
+    return (sliderWidth - visibleWidth) / clickCount;
+}
+
+function updateSliderPosition() {
+    sliderContainer.style.transform = `translateX(-${currentPosition}px)`;
+
+    leftButton.disabled = currentPosition <= 0;
+    rightButton.disabled = currentPosition >= sliderContainer.scrollWidth - sliderContainer.offsetWidth;
+}
+
+rightButton.addEventListener('click', function() {
+    const clickDistance = getClickDistance();
+    currentPosition += clickDistance;
+    if (currentPosition > sliderContainer.scrollWidth - sliderContainer.offsetWidth) {
+        currentPosition = sliderContainer.scrollWidth - sliderContainer.offsetWidth;
+    }
+    updateSliderPosition();
+});
+
+leftButton.addEventListener('click', function() {
+    const clickDistance = getClickDistance();
+    currentPosition -= clickDistance;
+    if (currentPosition < 0) {
+        currentPosition = 0;
+    }
+    updateSliderPosition();
+});
+
+updateSliderPosition();
